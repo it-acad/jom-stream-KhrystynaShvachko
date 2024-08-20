f8279526815aec1b4f4d837dcfba9f0f59321991
Khrystyna Shvacko <khrystyna.shvachko@gmail.com>
2024-08-20 18:02:05 +0300

    Task 5

--- src/main/java/jom/com/softserve/s6/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task5/MyUtils.java
@@ -3,10 +3,28 @@ package jom.com.softserve.s6.task5;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyUtils {
+    public static Stream<String> nameList(Map<String, Stream<String>> map) {
+        if (map == null) {
+            throw new NullPointerException("Map cannot be null");
+        }
 
-    //Write your code here
-
+        return map.values().stream()
+                .filter(Objects::nonNull)
+                .flatMap(stream -> stream
+                        .filter(Objects::nonNull)
+                        .map(String::trim)
+                        .filter(s -> !s.isEmpty())
+                        .map(s -> s.replaceAll("\\s+", " "))
+                        .map(s -> s.replaceAll("\\s", ""))
+                        .map(String::toLowerCase))
+                .distinct()
+                .sorted()
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .collect(Collectors.toList())
+                .stream();
+    }
 }
